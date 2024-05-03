0b4a96f5d792a43a21cef98811e9d0b855369268
FahadIbnaTuhin <fahadtuhin2@gmail.com>
2024-05-03 12:28:02 +0600

    update

--- z.cpp
+++ z.cpp
@@ -1,27 +1,12 @@
-// 4048451   Jul 11, 2013 3:40:15 AM	fuwutu	 242B - Big Segment	 GNU C++0x	Accepted	62 ms	0 KB
-#include <cstdio>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-    int n, l, r, minl, maxr, number(1);
-    scanf("%d%d%d", &n, &minl, &maxr);
-    for (int i = 2; i <= n; ++i)
-    {
-        scanf("%d%d", &l, &r);
-        if (l <= minl && r >= maxr)
-        {
-            number = i;
-        }
-        else if (l < minl || r > maxr)
-        {
-            number = -1;
-        }
-        minl = min(minl, l);
-        maxr = max(maxr, r);
-    }
-    printf("%d\n", number);
+int main() {
+    // ios::sync_with_stdio(false);
+    // cin.tie(0);
+
+
+    
     return 0;
 }
