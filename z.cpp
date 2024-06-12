976ed9cba54df55162331827e15a9ba22e54ba6d
FahadIbnaTuhin <fahadtuhin2@gmail.com>
2024-06-12 23:50:31 +0600

    update

--- z.cpp
+++ z.cpp
@@ -3,20 +3,23 @@
 using namespace std;
 
 int main() {
-    // ios::sync_with_stdio(false);
-    // cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-    string s = "<^>v<^>v<^"; // 3 to 7
-    string ss = "v<^>";
+    int l, r, a; cin >> l >> r >> a;
 
-    int ind = ss.find('<') + 3;
-    int n = 3;
+    while (a) {
+        if (l <= r) {
+            ++l;
+        } else {
+            ++r;
+        }
+        --a;
+    }
 
-    char a1, a2;
-    a1 = s[ind + 3];
-    a2 = s[ind - 3];
+    int ans = min(l, r) * 2;
 
-    cout << a1 << ' ' << a2 << '\n';
+    cout << ans << '\n';
 
     return 0;
 }
