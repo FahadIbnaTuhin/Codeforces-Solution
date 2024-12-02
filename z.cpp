9b9852fe30f1c4fe25cff92876dd540d98636955
FahadIbnaTuhin <fahadtuhin2@gmail.com>
2024-12-03 00:20:49 +0600

    update

--- z.cpp
+++ z.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
-    int t; cin >> t;
-    while (t--) {
-        
+    // ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    for (int i = 0; i < 5; i++) {
+        cout << i << '\n';
     }
+
 
     return 0;
 }
